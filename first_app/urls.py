c07624ee62bb0a96e413fdfa499c49d47047ee43
Alex Urtubia <alex.urtubia.contreras@gmail.com>
2021-07-24 12:55:28 -0400

    first version

---
+++ first_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path     
+from .views import roots, index, new, create, show, edit, destroy
+
+urlpatterns = [
+    path('', roots),
+    path('blogs/', index),
+    path('blogs/new/', new),
+    path('blogs/create/', create),
+    path('blogs/<int:number>/', show),
+    path('blogs/<int:number>/edit/', edit),
+    path('blogs/<int:number>/delete/', destroy),
+]
